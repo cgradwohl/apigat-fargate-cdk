760e06811b4bc1e1d71572175024cd47b10d1a43
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2022-10-08 13:27:25 -0700

    filename changes.

--- bin/app.ts
+++ bin/app.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { ApigatFargateCdkStack } from "../lib/apigat-fargate-cdk-stack";
+import { MainCdkStack } from "../lib/main";
 
 const app = new cdk.App();
-new ApigatFargateCdkStack(app, "ApigatFargateCdkStack", {
+new MainCdkStack(app, "ApigatFargateCdkStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
